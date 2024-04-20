d7e0a6e11f28d2c0df220fd55b93db67d64640b8
Olamideod <oluwamusiwaolamide@gmail.com>
2024-04-20 08:57:17 -0700

    0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,14 @@
-# instal the_sky_is_the_limit_no
-exec { 'update ulimit':
-  command  => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 15000\"/' /etc/default/nginx",
-  provider => 'shell',
+# Increases the amount of traffic an Nginx server can handle
+
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin'
 }
 
--> exec { 'restart':
-  command  => 'service nginx restart',
-  provider => 'shell',
+#restart Nginx
+
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
